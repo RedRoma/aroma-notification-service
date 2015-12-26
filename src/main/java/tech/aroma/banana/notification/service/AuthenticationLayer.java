506c6ba6d2e4e1b257e3aa91c2a3693e2556e51b
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-26 11:32:53 -0800

    Adding Authentication Layer

---
+++ src/main/java/tech/aroma/banana/notification/service/AuthenticationLayer.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2015 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ 
+package tech.aroma.banana.notification.service;
+
+
+import javax.inject.Inject;
+import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.aroma.banana.thrift.authentication.service.AuthenticationService;
+import tech.aroma.banana.thrift.exceptions.InvalidArgumentException;
+import tech.aroma.banana.thrift.exceptions.InvalidTokenException;
+import tech.aroma.banana.thrift.exceptions.OperationFailedException;
+import tech.aroma.banana.thrift.notification.service.NotificationService;
+import tech.aroma.banana.thrift.notification.service.SendNotificationRequest;
+import tech.aroma.banana.thrift.notification.service.SendNotificationResponse;
+import tech.sirwellington.alchemy.annotations.access.Internal;
+import tech.sirwellington.alchemy.annotations.designs.patterns.DecoratorPattern;
+
+import static tech.aroma.banana.thrift.functions.BananaAssertions.checkRequestNotNull;
+import static tech.aroma.banana.thrift.functions.BananaAssertions.legalToken;
+import static tech.aroma.banana.thrift.functions.BananaAssertions.validTokenIn;
+import static tech.sirwellington.alchemy.annotations.designs.patterns.DecoratorPattern.Role.CONCRETE_DECORATOR;
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
+
+/**
+ * This class decorates an existing {@linkplain NotificationService.Iface Notification Service},
+ * and adds Authentication of Tokens against an {@linkplain AuthenticationService.Iface Authentication Service}.
+ * 
+ * @author SirWellington
+ */
+@Internal
+@DecoratorPattern(role = CONCRETE_DECORATOR)
+final class AuthenticationLayer implements NotificationService.Iface
+{
+    private final static Logger LOG = LoggerFactory.getLogger(AuthenticationLayer.class);
+
+    private final AuthenticationService.Iface authenticationService;
+    private NotificationService.Iface delegate;
+    
+    @Inject
+    AuthenticationLayer(AuthenticationService.Iface authenticationService,
+                        NotificationService.Iface delegate)
+    {
+        checkThat(authenticationService, delegate)
+            .are(notNull());
+
+        this.authenticationService = authenticationService;
+        this.delegate = delegate;
+    }
+
+    
+    
+    @Override
+    public double getApiVersion() throws TException
+    {
+        return delegate.getApiVersion();
+    }
+
+    @Override
+    public SendNotificationResponse sendNotification(SendNotificationRequest request) throws InvalidArgumentException,
+                                                                                             OperationFailedException,
+                                                                                             InvalidTokenException, 
+                                                                                             TException
+    {
+        checkRequestNotNull(request);
+        
+        checkThat(request.token)
+            .throwing(InvalidTokenException.class)
+            .is(legalToken())
+            .is(validTokenIn(authenticationService));
+        
+        return delegate.sendNotification(request);
+    }
+
+}
---
+++ src/test/java/tech/aroma/banana/notification/service/AuthenticationLayerTest.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2015 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.banana.notification.service;
+
+import org.apache.thrift.TException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import tech.aroma.banana.thrift.authentication.AuthenticationToken;
+import tech.aroma.banana.thrift.authentication.UserToken;
+import tech.aroma.banana.thrift.authentication.service.AuthenticationService;
+import tech.aroma.banana.thrift.authentication.service.VerifyTokenRequest;
+import tech.aroma.banana.thrift.exceptions.InvalidTokenException;
+import tech.aroma.banana.thrift.exceptions.OperationFailedException;
+import tech.aroma.banana.thrift.notification.service.NotificationService;
+import tech.aroma.banana.thrift.notification.service.SendNotificationRequest;
+import tech.aroma.banana.thrift.notification.service.SendNotificationResponse;
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
+import tech.sirwellington.alchemy.test.junit.runners.DontRepeat;
+import tech.sirwellington.alchemy.test.junit.runners.GeneratePojo;
+import tech.sirwellington.alchemy.test.junit.runners.GenerateString;
+import tech.sirwellington.alchemy.test.junit.runners.Repeat;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.NumberGenerators.doubles;
+import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
+import static tech.sirwellington.alchemy.test.junit.runners.GenerateString.Type.HEXADECIMAL;
+
+
+/**
+ *
+ * @author SirWellington
+ */
+@Repeat(100)
+@RunWith(AlchemyTestRunner.class)
+public class AuthenticationLayerTest 
+{
+
+    @Mock
+    private AuthenticationService.Iface authenticationService;
+    
+    @Mock
+    private NotificationService.Iface delegate;
+    
+    @GeneratePojo
+    private SendNotificationRequest sendNotificationRequest;
+    @GeneratePojo
+    private SendNotificationResponse sendNotificationResponse;
+    
+    @GenerateString(HEXADECIMAL)
+    private String tokenId;
+    
+    private AuthenticationToken token;
+    
+    private AuthenticationLayer instance;
+    
+    @Before
+    public void setUp() throws TException
+    {
+        instance = new AuthenticationLayer(authenticationService, delegate);
+        
+        verifyZeroInteractions(authenticationService, delegate);
+        
+        when(delegate.sendNotification(sendNotificationRequest))
+            .thenReturn(sendNotificationResponse);
+        
+        UserToken userToken = new UserToken()
+        .setTokenId(tokenId);
+        token = new AuthenticationToken();
+        token.setUserToken(userToken);
+        
+        sendNotificationRequest.token = token;
+    }
+
+    @DontRepeat
+    @Test
+    public void testConstructor() throws Exception
+    {
+        assertThrows(() -> new AuthenticationLayer(null, delegate))
+            .isInstanceOf(IllegalArgumentException.class);
+        
+        assertThrows(() -> new AuthenticationLayer(authenticationService, null))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    
+    @Test
+    public void testGetApiVersion() throws Exception
+    {
+        double expected = one(doubles(1.0, 10.0));
+        when(delegate.getApiVersion()).thenReturn(expected);
+        
+        double result = instance.getApiVersion();
+        assertThat(result, is(expected));
+        verify(delegate).getApiVersion();
+    }
+
+    @Test
+    public void testSendNotification() throws Exception
+    {
+    }
+    
+    @Test
+    public void testSendNotificationWithInvalidToken() throws Exception
+    {
+        VerifyTokenRequest request = new VerifyTokenRequest()
+            .setTokenId(tokenId);
+        
+        when(authenticationService.verifyToken(request))
+            .thenThrow(new InvalidTokenException());
+        
+        assertThrows(() -> instance.sendNotification(sendNotificationRequest))
+            .isInstanceOf(InvalidTokenException.class);
+    }
+    
+    @Test
+    public void testSendNotificationWhenDelegateThrows() throws Exception
+    {
+        when(delegate.sendNotification(sendNotificationRequest))
+            .thenThrow(new OperationFailedException());
+        
+        assertThrows(() -> instance.sendNotification(sendNotificationRequest))
+            .isInstanceOf(OperationFailedException.class);
+    }
+
+}
