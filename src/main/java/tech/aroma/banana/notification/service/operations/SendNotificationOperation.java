18bab86e2f3e51f9b7a2d8b881c72048fe3e52e9
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-26 12:43:01 -0800

    SendNotificationOperation updates
    + Adding actual business logic

--- src/main/java/tech/aroma/banana/notification/service/operations/SendNotificationOperation.java
+++ src/main/java/tech/aroma/banana/notification/service/operations/SendNotificationOperation.java
@@ -16,35 +16,87 @@
 
 package tech.aroma.banana.notification.service.operations;
 
+import javax.inject.Inject;
+import org.apache.thrift.TBase;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import tech.aroma.banana.notification.service.pigeon.Pigeon;
+import tech.aroma.banana.notification.service.pigeon.PigeonFactory;
+import tech.aroma.banana.thrift.channels.BananaChannel;
 import tech.aroma.banana.thrift.notification.service.SendNotificationRequest;
 import tech.aroma.banana.thrift.notification.service.SendNotificationResponse;
+import tech.sirwellington.alchemy.annotations.access.Internal;
+import tech.sirwellington.alchemy.annotations.designs.patterns.StrategyPattern;
 import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
 
+import static tech.aroma.banana.notification.service.NotificationAssertions.validEvent;
 import static tech.aroma.banana.thrift.functions.BananaAssertions.checkNotNull;
+import static tech.aroma.banana.thrift.functions.BananaAssertions.withMessage;
+import static tech.sirwellington.alchemy.annotations.designs.patterns.StrategyPattern.Role.CLIENT;
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
+import static tech.sirwellington.alchemy.arguments.assertions.CollectionAssertions.nonEmptyList;
 import static tech.sirwellington.alchemy.generator.ObjectGenerators.pojos;
 
 /**
  *
  * @author SirWellington
  */
+@Internal
+
 final class SendNotificationOperation implements ThriftOperation<SendNotificationRequest, SendNotificationResponse>
 {
 
     private final static Logger LOG = LoggerFactory.getLogger(SendNotificationOperation.class);
+    
+    @StrategyPattern(role = CLIENT)
+    private final PigeonFactory pigeonFactory;
 
+    @Inject
+    SendNotificationOperation(PigeonFactory pigeonFactory)
+    {
+        checkThat(pigeonFactory)
+            .is(notNull());
+        
+        this.pigeonFactory = pigeonFactory;
+    }
+    
+    
     @Override
     public SendNotificationResponse process(SendNotificationRequest request) throws TException
     {
         checkNotNull(request, "request is missing");
         
+        checkThat(request.event)
+            .throwing(withMessage("Invalid Event Type"))
+            .is(validEvent());
+        
+        checkThat(request.channels)
+            .throwing(withMessage("missing Channels to send notifications to"))
+            .is(nonEmptyList());
+        
         // For each Channel in the request...
         // Obtain a 'courier' or a 'pigeon' type from a factory.
-        // send the event to the pigeon
-        
+        // Send the pigeon off to send the event
         //Each pigeon is responsible for sending the message to the right place
+        int success = 0;
+        for(BananaChannel channel : request.channels)
+        {
+            Pigeon<? super TBase> pigeon = pigeonFactory.getPigeonFor(channel);
+            
+            if (pigeon == null)
+            {
+                LOG.warn("Pigeon Factory returned null");
+                continue;
+            }
+            
+            LOG.debug("Sending pigeon {} to deliver event {}", pigeon, request.event);
+            pigeon.deliverMessageTo(request.event, channel);
+            success++;
+        }
+        
+        LOG.debug("Sent event {} to {} channels", request.event, success);
         
         SendNotificationResponse response = pojos(SendNotificationResponse.class).get();
         
--- src/test/java/tech/aroma/banana/notification/service/operations/SendNotificationOperationTest.java
+++ src/test/java/tech/aroma/banana/notification/service/operations/SendNotificationOperationTest.java
@@ -16,9 +16,15 @@
 
 package tech.aroma.banana.notification.service.operations;
 
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import tech.aroma.banana.notification.service.pigeon.Pigeon;
+import tech.aroma.banana.notification.service.pigeon.PigeonFactory;
+import tech.aroma.banana.thrift.channels.BananaChannel;
 import tech.aroma.banana.thrift.exceptions.InvalidArgumentException;
 import tech.aroma.banana.thrift.notification.service.SendNotificationRequest;
 import tech.aroma.banana.thrift.notification.service.SendNotificationResponse;
@@ -29,8 +35,14 @@ import tech.sirwellington.alchemy.test.junit.runners.Repeat;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static tech.aroma.banana.notification.service.ChannelGenerators.channels;
+import static tech.aroma.banana.notification.service.EventGenerators.events;
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.CollectionGenerators.listOf;
 import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
-
 
 /**
  *
@@ -38,36 +50,75 @@ import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThr
  */
 @Repeat(100)
 @RunWith(AlchemyTestRunner.class)
-public class SendNotificationOperationTest 
+public class SendNotificationOperationTest
 {
+
+    @Mock
+    private PigeonFactory pigeonFactory;
     
+    @Mock
+    private Pigeon pigeon;
+
     @GeneratePojo
     private SendNotificationRequest request;
-    
+
     private SendNotificationOperation instance;
 
     @Before
     public void setUp()
     {
-        instance = new SendNotificationOperation();
+        instance = new SendNotificationOperation(pigeonFactory);
         
+        verifyZeroInteractions(pigeonFactory);
+        
+        List<BananaChannel> channels = listOf(channels());
+        request.setChannels(channels);
+        request.setEvent(one(events()));
     }
     
+    @DontRepeat
+    @Test
+    public void testConstructor()
+    {
+        assertThrows(() -> new SendNotificationOperation(null))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @Test
     public void testProcess() throws Exception
     {
         SendNotificationResponse response = instance.process(request);
-        
+
         assertThat(response, notNullValue());
         
+        verify(pigeonFactory, times(request.channels.size()))
+            .getPigeonFor(Mockito.any());
     }
-    
+
     @DontRepeat
     @Test
     public void testProcessWithBadRequest() throws Exception
     {
         assertThrows(() -> instance.process(null))
             .isInstanceOf(InvalidArgumentException.class);
+
+        SendNotificationRequest empty = new SendNotificationRequest();
+
+        assertThrows(() -> instance.process(empty))
+            .isInstanceOf(InvalidArgumentException.class);
+
+        SendNotificationRequest noEvent = new SendNotificationRequest()
+            .setChannels(request.channels);
+
+        assertThrows(() -> instance.process(noEvent))
+            .isInstanceOf(InvalidArgumentException.class);
+
+        SendNotificationRequest noChannels = new SendNotificationRequest()
+            .setEvent(request.event);
+
+        assertThrows(() -> instance.process(noChannels))
+            .isInstanceOf(InvalidArgumentException.class);
+
     }
-    
+
 }
